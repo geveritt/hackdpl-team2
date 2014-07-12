34b12b24c1427813e470cc3cfcb23acde2af7ff0
jTurnerCPW <jackson.turner@compuware.com>
2014-07-12 04:12:56 -0400

    adding location detection, and closest branch locator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 //= require foundation
 //= require_tree .
+//= require index
 
-$(function(){ $(document).foundation(); });
+$(function(){ $(document).foundation(); initHome();});
