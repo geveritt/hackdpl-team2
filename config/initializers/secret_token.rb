473ab6e3ed8b3ed92f1acbc4e72ce3fff31d1fdf
Timothy Fisher <timothyf@gmail.com>
2014-07-11 23:03:04 -0400

    changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackdpl::Application.config.secret_token = '14fdd0a4e5207e9573843f66fbd44b7f0e39645600597814ddf39d22229d8bfd53af495f41f0c8e91892ee9108c1b53aaf5fea555e6d79b824ade2cd6bd93119'
