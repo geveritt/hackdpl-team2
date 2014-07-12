2a7c49bc51316f172aee7822f92bb662ecdb89a3
Timothy Fisher <timothyf@gmail.com>
2014-07-12 02:16:43 -0400

    changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 class Event
   
   def self.import_feed
-    feed = Feedjira::Feed.fetch_and_parse('http://www.detroit.lib.mi.us/events/rss.xml')
+    #feed = Feedjira::Feed.fetch_and_parse('http://www.detroit.lib.mi.us/events/rss.xml')
+    feed = Feedjira::Feed.fetch_and_parse('http://www.detroit.lib.mi.us/events/rss.xml/144')
     feed.entries
   end
   
 end
+
